7ba622b1274e616e7d854425d569cc45940cf46b
Jacqueline <composercookie@gmail.com>
2016-04-16 23:22:21 -0700

    pre-tools

---
+++ EGMapEditor/ToolType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGMapEditor
+{
+    enum ToolType
+    {
+        Pencil,
+        Ellipse,
+        Eraser,
+        Brush,
+        Bucket,
+        AreaRect,
+        AreaCircle,
+        Flipper,
+        MagicWand,
+        TilePicker
+    }
+}
