b5a1f22ec7889df23c8aa4f6b6a509a22f938ea8
Jacqueline <composercookie@gmail.com>
2016-04-19 20:07:02 -0700

    nightly push

---
+++ EGMapEditor/AreaTool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGMapEditor
+{
+    class AreaTool : Tools
+    {
+        public ToolType ToolType { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public List<KeyValuePair<int, int>> Use()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
