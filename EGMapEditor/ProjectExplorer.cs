a0be7d75af0e132850d6c0da410f83839a0f1b5d
Jacqueline <composercookie@gmail.com>
2016-04-25 19:58:31 -0700

    Explorer begin

--- EGMapEditor/ProjectExplorer.cs
+++ EGMapEditor/ProjectExplorer.cs
@@ -1,12 +1,50 @@
 ﻿using System.Windows.Forms.DockPanel;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace EGMapEditor
 {
     public partial class ProjectExplorer : DockContent
     {
+        private List<TreeNode> networkMaps;
+        private List<TreeNode> localMaps;
+        private List<TreeNode> sessionMaps;
+
+
         public ProjectExplorer()
         {
             InitializeComponent();
+            networkMaps = new List<TreeNode>();
+            localMaps = new List<TreeNode>();
+            sessionMaps = new List<TreeNode>();
+        }
+
+        public void LoadExplorer()
+        {
+            trvExplorer.Nodes.Clear();
+            trvExplorer.Nodes.Add("Network Maps: ");
+            LoadNetworkMaps();
+            trvExplorer.Nodes.Add("Local Maps: ");
+            LoadLocalMaps();
+            trvExplorer.Nodes.Add("Session Maps: ");
+            LoadSessionMaps();
+        }
+
+        public void LoadNetworkMaps()
+        {
+            networkMaps.Clear();
+
+            trvExplorer.Nodes[0].Nodes.AddRange(networkMaps.ToArray());
+        }
+
+        public void LoadLocalMaps()
+        {
+            localMaps.Clear();
+        }
+
+        public void LoadSessionMaps()
+        {
+            sessionMaps.Clear();
         }
     }
 }
