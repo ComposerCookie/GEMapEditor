7207105bee43f8ae83e3dff0c8ba609dae5036bc
Jacqueline <composercookie@gmail.com>
2016-05-11 13:28:47 -0700

    Pre data

---
+++ EGMapEditor/CustomProperty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGMapEditor
+{
+    class CustomProperty<T>
+    {
+        T Value { get; set; }
+
+        public CustomProperty(T value)
+        {
+            Value = value;
+        }
+    }
+}
---
+++ EGMapEditor/Propertiable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGMapEditor
+{
+    interface Propertiable<T>
+    {
+        List<CustomProperty<T>> CustomProperties { get; set; }
+
+        void AddProperty(T type, T value);
+        void RemoveProperty(int index);
+        void EditProperty(int index, T value);
+        CustomProperty<T> GetProperty(int index);
+    }
+}
